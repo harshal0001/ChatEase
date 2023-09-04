0ac5cbbf5c3d21e48e45b037076df6f90484d1b5
Harshal <86665758+harshal0001@users.noreply.github.com>
2023-09-04 16:25:27 +0530

    Project Initiated

---
+++ android/app/src/main/kotlin/com/example/chat_ease/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_ease
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/auth.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                child: Image.asset('assets/images/chat_ease.png'),
+                width: 200,
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            decoration:
+                                InputDecoration(labelText: 'Email Address'),
+                          ),
+                          TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(labelText: 'Password'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
